df9acd7c67ae1194d822702aa20c32dd6a9b3ad0
Ulyana <ushinkevich@mail.ru>
2023-04-26 23:47:23 +0400

    task #3

--- hw-3.js
+++ hw-3.js
@@ -10,3 +10,12 @@ if (c > 0 && c < 10) {
 } else {
     console.log ('Неверно');
 }
+
+//#3
+let d = Number (999);
+let e = Number (55);
+if (d > 100 || e > 100) {
+    console.log ('Верно');
+} else {
+    console.log ('Неверно');
+}

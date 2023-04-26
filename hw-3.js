08103169585b235406b46c7b930f6e5f25a2a1b9
Ulyana <ushinkevich@mail.ru>
2023-04-26 23:35:56 +0400

    task #2

--- hw-3.js
+++ hw-3.js
@@ -3,4 +3,10 @@ let password = 'пароль';
 let conclusion = prompt ('Введите пароль');
 (conclusion === password) ? alert ('Пароль введен верно') : alert ('Пароль введен неправильно');
  
-
+//#2
+let c = Number (-2);
+if (c > 0 && c < 10) {
+    console.log ('Верно');
+} else {
+    console.log ('Неверно');
+}

a3beffaae45ca50d74a5d5320eca3a58702bd3c0
Ulyana <ushinkevich@mail.ru>
2023-04-27 00:08:27 +0400

    task #5

--- hw-3.js
+++ hw-3.js
@@ -28,3 +28,48 @@ a = number1;
 let number2 = 5 - b;
 b = number2;
 alert(a + b);
+
+//#5
+let monthNumber = prompt ('Введите номер месяца')
+switch (monthNumber) {
+    case '1':
+        console.log ('Зима');
+        break;
+    case '2':
+        console.log ('Зима');
+        break;
+    case '3':
+        console.log ('Весна');
+        break;
+    case '4':
+        console.log ('Весна');
+        break;   
+    case '5':
+        console.log ('Весна');
+        break;
+    case '6':
+        console.log ('Лето');
+        break;
+    case '7':
+        console.log ('Лето');
+        break;
+    case '8':
+        console.log ('Лето');
+        break;
+    case '9':
+        console.log ('Осень');
+        break;
+    case '10':
+        console.log ('Осень');
+        break; 
+    case '11':
+        console.log ('Осень');
+        break;  
+    case '12':
+        console.log ('Зима');
+        break;
+         
+    default:
+        console.log ('Упс.. Ввел что-то не то')
+        break;
+}

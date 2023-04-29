2f84c09743676017c57ff9f20b50e6b59a51e987
Ulyana <ushinkevich@mail.ru>
2023-04-29 12:24:47 +0400

    task #4

--- hw-4.js
+++ hw-4.js
@@ -12,7 +12,7 @@ do {
 let a = 1;
 
 while (a <= 5) {
-    console.log (a);
+    console.log (a); //1 2 3 4 5
     a++
 }
 
@@ -20,5 +20,17 @@ while (a <= 5) {
 
 for (let num = 7; num <= 22; num++) {
     
-    console.log (num); 
+    console.log (num); // 7 8 9...22
+}
+
+//#4
+
+let salary = {
+    'Коля': 200,
+    'Вася': 300,
+    'Петя': 400,
+}
+
+for (const key in salary) {
+    console.log ( `${key} - зарплата ${salary[key]} `)
 }

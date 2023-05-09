86434b6d9d0395e223d2bdea744e50daf13487e7
Ulyana <ushinkevich@mail.ru>
2023-05-09 12:25:11 +0400

    task #1

---
+++ hw-5.js
@@ -0,0 +1,12 @@
+//#1
+
+function comparison (a, b) {
+    if (a > b) {
+        console.log (b); 
+    } else {
+        console.log (a);                                        
+    }
+}
+
+ comparison (8, 4);
+ comparison (6, 6);

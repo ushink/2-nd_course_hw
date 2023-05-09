e73a9a644e260ddfd02b4cb3df0da98e18bc0788
Ulyana <ushinkevich@mail.ru>
2023-05-09 12:58:55 +0400

    task #3

--- hw-5.js
+++ hw-5.js
@@ -13,14 +13,27 @@ function comparison(a, b) {
 
  //#2
 
- function number(c) {
+function number(c) {
     if (c % 2 == 0) {
         console.log ('Число четное');
     } else {
         console.log ('Число нечетное');
     }
- }
+}
 
  number (5); // Число нечетное
  number (80); // Число четное
  number (0); // Число четное
+
+ //#3.1
+
+let square1 = function (n) { 
+        return n * n ;
+}
+console.log (square1(6)); // 36
+
+ //#3.2
+
+const square2 = (m) => m * m ;
+
+console.log (square2(5));

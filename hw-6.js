066ba496a58bfb897c9547b2b5b89b54d4f41f54
Ulyana <ushinkevich@mail.ru>
2023-05-13 18:02:56 +0400

    task #2

--- hw-6.js
+++ hw-6.js
@@ -7,3 +7,13 @@ for (let a = 0; a < numbers.length; a++) {
     console.log (numbers[a]);
     
 }
+
+//#2
+
+const numbers2 = [1, 5, 4, 10, 0, 3];
+
+numbers2.forEach((el, index) => {
+    if (el == 4) {
+        console.log (index);
+    }
+});

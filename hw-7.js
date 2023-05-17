e4c80fc2345bae06ce8006841a514e107b3ac418
Ulyana <ushinkevich@mail.ru>
2023-05-17 17:11:29 +0400

    #2

--- hw-7.js
+++ hw-7.js
@@ -3,4 +3,14 @@
  let str = 'js';
  console.log(str.toUpperCase());
 
- 
+ //#2
+
+ function searchStart(arr, str) {
+    let pattern = `^${str}`;
+    let re = new RegExp(pattern);
+    return arr.map(e => e.toLowerCase()).filter((it) => re.exec(it));
+    
+ }
+console.log (searchStart(['Кошка', 'Кит', 'Комар', 'Носорог'], 'ко')); // ['кошка', 'комар']
+console.log (searchStart(['яблоко', 'груша', 'гриб', 'огурец'], 'гру')); // ['груша'])
+console.log (searchStart(['Дом', 'Банк', 'Больница', 'Театр'], 'Кино')); // [])

b34c90c3a330220f6b6ca9fd7c9a76d9dffb2992
Ulyana <ushinkevich@mail.ru>
2023-05-17 17:18:37 +0400

    #3

--- hw-7.js
+++ hw-7.js
@@ -14,3 +14,12 @@
 console.log (searchStart(['Кошка', 'Кит', 'Комар', 'Носорог'], 'ко')); // ['кошка', 'комар']
 console.log (searchStart(['яблоко', 'груша', 'гриб', 'огурец'], 'гру')); // ['груша'])
 console.log (searchStart(['Дом', 'Банк', 'Больница', 'Театр'], 'Кино')); // [])
+
+//#3
+
+let num = 32.58884;
+
+console.log(Math.floor(num)); // 32 ; До меньшего целого
+console.log(Math.ceil(num)); // 33 ; До большего целого
+console.log(Math.round(num)); // 33 ; До ближайшего целого
+

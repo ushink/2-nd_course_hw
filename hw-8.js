911b433390471795a99b2bcf55786a6c6dee61ca
Ulyana <ushinkevich@mail.ru>
2023-05-21 12:57:44 +0400

    #1

--- hw-8.js
+++ hw-8.js
@@ -1,1 +1,29 @@
+//#1
 
+const people = [
+    { name: 'Глеб', age: 29 },
+    { name: 'Анна', age: 17 },
+    { name: 'Олег', age: 7 },
+    { name: 'Оксана', age: 47 }
+ ];
+ 
+ console.log(people.sort(function (a, b) {
+    if (a.age > b.age) {
+        return 1;
+
+    }
+    if (a.age < b.age) {
+        return -1;
+
+    }
+
+    return 0;
+ }));
+
+ // код выше должен вывеcти =>
+ // [
+ //  { name: 'Олег', age: 7 },
+ //  { name: 'Анна', age: 17 },
+ //  { name: 'Глеб', age: 29 },
+ //  { name: 'Оксана', age: 47 }
+ // ]

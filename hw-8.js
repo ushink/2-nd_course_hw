bdf4c020a811716e68f28c46a561039d3422b628
Ulyana <ushinkevich@mail.ru>
2023-05-21 14:07:46 +0400

    #2

--- hw-8.js
+++ hw-8.js
@@ -27,3 +27,42 @@ const people = [
  //  { name: 'Глеб', age: 29 },
  //  { name: 'Оксана', age: 47 }
  // ]
+
+
+ //#2
+
+function isPositive(number) {
+    if (number > 0) {
+        return number;
+    }
+}
+
+function isMale(whoIS) {
+    if (whoIS.gender == 'male') {
+        return whoIS;
+    }    
+}
+
+function filter(arr, ruleFunction) {
+    const output = [];
+
+    for (let i = 0; i < arr.length; i++) {
+    output.push(ruleFunction(arr[i]));
+    }
+
+    return output;
+}
+
+    
+    console.log(filter([3, -4, 1, 9], isPositive));
+     // Должен выводить [3, 1, 9]
+    
+    const people2 = [
+       {name: 'Глеб', gender: 'male'},
+       {name: 'Анна', gender: 'female'},
+       {name: 'Олег', gender: 'male'},
+       {name: 'Оксана', gender: 'female'}
+    ];
+    
+    console.log(filter(people2, isMale)); 
+    // Должен выводить [{name: 'Глеб', gender: 'male'},  {name: 'Олег', gender: 'male'}]
